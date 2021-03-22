ce6084edcc0e87d561c845779ffc0378d7d50a18
ficsor <ficsor.lajos@gmail.com>
2021-03-22 09:14:32 +0100

    A 7. gyakorlaton módosítva: 4.1 a, b, c, d - gyakorló feladat stílusban implementálva

--- Het5BI2/src/alkalmazott/Alkalmazott.java
+++ Het5BI2/src/alkalmazott/Alkalmazott.java
@@ -6,6 +6,7 @@ package alkalmazott;
 public class Alkalmazott {
 	private final String nev;
 	private int fizetes;
+	private final static int SZJAKULCS = 15;
 		
 	public Alkalmazott(String nev, int fizetes) {
 		super();
@@ -25,6 +26,19 @@ public class Alkalmazott {
 		fizetes = (int)(fizetes + fizetes*szazalek/100); 
 	}
 	
+	public int getSzja() {
+		return (int)(fizetes * SZJAKULCS / 100.);
+	}
+	
+	public boolean nagyobbE(Alkalmazott masik) {
+		return fizetes > masik.fizetes;
+	}
+	
+	@Override
+	public String toString() {
+		return "[nev=" + nev + ", fizetes=" + fizetes + "]";
+	}
+	// Barkács megoldás!
 	public String kiirInfo() {
 		return nev + ": " + fizetes;
 	}
--- Het5BI2/src/alkalmazott/AlkalmazottApp.java
+++ Het5BI2/src/alkalmazott/AlkalmazottApp.java
@@ -1,5 +1,5 @@
 package alkalmazott;
-/* 4.1 b feladat
+/* 4.1 b, c, d feladat
  * Ez már elfogadható
  * 
  */
@@ -8,12 +8,59 @@ public class AlkalmazottApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Alkalmazott[] alkalmazottak = new Alkalmazott[5];
+		feltolt(alkalmazottak);
+		listaz(alkalmazottak);
+		System.out.println("Átlagfizetés: " + atlagfizetes(alkalmazottak));
+		System.out.println("SZJA: " + szjaOsszeg(alkalmazottak));
+		System.out.println("Legnagyobb fizetés: " + legnagyobbFizetes(alkalmazottak));
+/*
+ * 
 		Alkalmazott alkalmazott = new Alkalmazott("Józsi", 100000);
+		System.out.println(alkalmazott);  //alkalmazott.toString()
 		System.out.println(alkalmazott.kiirInfo());
 		alkalmazott.setFizetes(110000);
 		System.out.println(alkalmazott.kiirInfo());
 		alkalmazott.fizetestNovel(20);
 		System.out.println(alkalmazott.kiirInfo());
+*/
 	}
 
+	public static void feltolt(Alkalmazott[] alk) {
+		for (int i=0; i<alk.length; i++) {
+			alk[i] = new Alkalmazott("Béla" + (i+1), (i+1)*100000);
+		}
+	}
+	
+	public static void listaz(Alkalmazott[] alk) {
+		for (Alkalmazott elem : alk) {
+			System.out.println(elem);
+		}
+	}
+	
+	public static int atlagfizetes(Alkalmazott[] alk) {
+		int osszeg = 0;
+		for (Alkalmazott elem : alk) {
+			osszeg += elem.getFizetes();
+		}
+		return osszeg / alk.length;
+	}
+	
+	public static int szjaOsszeg(Alkalmazott[] alk) {
+		int osszeg = 0;
+		for (Alkalmazott elem : alk) {
+			osszeg += elem.getSzja() ;
+		}
+		return osszeg;
+	}
+	
+	public static Alkalmazott legnagyobbFizetes(Alkalmazott[] alk) {
+		int maxindex = 0;
+		for (int i=1;  i<alk.length; i++) {
+			if (alk[i].getFizetes() > alk[maxindex].getFizetes()) {
+				maxindex = i;
+			}
+		}
+		return alk[maxindex];
+	}
 }
