e9717c38c7655696a41d4c715da1bfcac0f8bc4a
ficsor <ficsor.lajos@gmail.com>
2021-04-19 13:32:20 +0200

    9.1 feladat elkezdve (EgyetemiAlkalmazott osztály még hiányos!)

---
+++ Het11BI2-3/src/szemelyek/Alkalmazott.java
@@ -0,0 +1,30 @@
+package szemelyek;
+
+public class Alkalmazott extends Szemely {
+	private String munkahely;
+	private int fizetes;
+	public Alkalmazott(String nev, int kor, String munkahely, int fizetes) {
+		super(nev, kor);
+		this.munkahely = munkahely;
+		this.fizetes = fizetes;
+	}
+	@Override
+	public String toString() {
+		return "Alkalmazott [munkahely=" + munkahely + ", fizetes=" + fizetes + ", toString()=" + super.toString()
+				+ "]";
+	}
+	public String getMunkahely() {
+		return munkahely;
+	}
+	public void setMunkahely(String munkahely) {
+		this.munkahely = munkahely;
+	}
+	public int getFizetes() {
+		return fizetes;
+	}
+	public void setFizetes(int fizetes) {
+		this.fizetes = fizetes;
+	}
+	
+	
+}
---
+++ Het11BI2-3/src/szemelyek/EgyetemiAlkalmazott.java
@@ -0,0 +1,7 @@
+package szemelyek;
+
+public class EgyetemiAlkalmazott extends Alkalmazott {
+	private static int alapber = 500000;
+	private static enum beosztas {PROF, OKTATO, ADMIN};
+
+}
